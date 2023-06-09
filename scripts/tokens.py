356e088d1cb618d5d7d79d2761e35308e77b04b4
Bob Hua <schua1013@gmail.com>
2023-06-09 17:22:00 -0400

    add openai test scripts

---
+++ scripts/tokens.py
@@ -0,0 +1,16 @@
+import tiktoken
+import os
+if __name__ == "__main__":
+    model = "gpt-3.5-turbo"
+    encoding = tiktoken.encoding_for_model(model)
+    file = os.path.join(os.path.dirname(__file__),"prompt.txt")
+    with open(file, "r") as f:
+        prompt = f.read()
+    encoded = encoding.encode(prompt)
+    
+    print(f"Number of words in prompt: {len(prompt.split())}")
+    print(f"Number of characters in prompt: {len(prompt)}")
+    print(f"Number of tokens: {len(encoded)}")
+
+
+
